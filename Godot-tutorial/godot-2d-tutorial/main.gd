f9052d4622a90a93848131b973c20dc7378f8889
ZoNaDak <kbh3719net@gmail.com>
2023-10-12 00:33:38 +0900

    Heads up display

--- Godot-tutorial/godot-2d-tutorial/main.gd
+++ Godot-tutorial/godot-2d-tutorial/main.gd
@@ -3,28 +3,26 @@ extends Node2D
 @export var mob_scene: PackedScene
 var score
 
-func _ready():
-	new_game()
-
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
-
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_score_timer_timeout():
 	score += 1
-
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
 	$ScoreTimer.start()
-
 
 func _on_mob_timer_timeout():
 	var mob = mob_scene.instantiate()
